0ca48d832fd69b98ed68fde8c9660f51978f5e14
krmanik <12841290+krmanik@users.noreply.github.com>
2022-09-06 16:41:36 +0530

    fabricjs tools for creating basic shapes

---
+++ ts/image-occlusion/icons.ts
@@ -0,0 +1,11 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+/// <reference types="../lib/image-import" />
+
+export { default as mdiCursorDefaultOutline } from "@mdi/svg/svg/cursor-default-outline.svg";
+export { default as mdiMagnify } from "@mdi/svg/svg/magnify.svg";
+export { default as mdiRectangleOutline } from "@mdi/svg/svg/rectangle-outline.svg";
+export { default as mdiCircleOutline } from "@mdi/svg/svg/circle-outline.svg";
+export { default as mdiDeleteOutline } from "@mdi/svg/svg/delete-outline.svg";
+export { default as mdiPalette } from "@mdi/svg/svg/palette.svg";
---
+++ ts/image-occlusion/tool-buttons.ts
@@ -0,0 +1,35 @@
+import {
+    mdiCursorDefaultOutline,
+    mdiMagnify,
+    mdiRectangleOutline,
+    mdiCircleOutline,
+    mdiDeleteOutline,
+    mdiPalette,
+} from "./icons";
+
+export let tools = [
+    {
+        id: "cursor",
+        icon: mdiCursorDefaultOutline,
+    },
+    {
+        id: "magnify",
+        icon: mdiMagnify,
+    },
+    {
+        id: "draw-rectangle",
+        icon: mdiRectangleOutline,
+    },
+    {
+        id: "draw-circle",
+        icon: mdiCircleOutline,
+    },
+    {
+        id: "delete-item",
+        icon: mdiDeleteOutline,
+    },
+    {
+        id: "choose-color",
+        icon: mdiPalette,
+    },
+];
